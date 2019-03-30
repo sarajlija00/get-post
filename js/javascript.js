bb5b320ad6a647568dc8118f59765d940e10485b
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-03-30 13:35:17 +0100

    correction

--- js/javascript.js
+++ js/javascript.js
@@ -11,12 +11,12 @@ function get() {
 
 function post () {
     let xhttp = new XMLHttpRequest ();
-    xhttp.onreadystatechange = function {
+    xhttp.onreadystatechange = function() {
         if(this.readyState == 4 && this.status ==200) {
             let obj = JSON.parse (xhttp.responseText);
             document.getElementById('result').innerHTML = obj.message;
         }
-    }
+    };
     xhttp.open("POST",'https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/hello', true);
     xhttp.setRequestHeader ("Content-Type", "application/json;charset=UTF-8");
 

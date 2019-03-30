9fbd0c76e3fe05777ffb8411becef04ee1a8055c
Elvir Uzunovic <uzunovicelvir@gmail.com>
2019-03-30 07:47:32 +0100

    add post

--- js/javascript.js
+++ js/javascript.js
@@ -5,10 +5,22 @@ function get() {
             document.getElementById ('text').innerHTML = xhttp.responseText;
         }
     }
-    xhttp.open ("get", "https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/hello", true);
+    xhttp.open ("GET", 'https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/hello', true);
     xhttp.send ();
 }
 
 function post () {
+    let xhttp = new XMLHttpRequest ();
+    xhttp.onreadystatechange = function {
+        if(this.readyState == 4 && this.status ==200) {
+            let obj = JSON.parse (xhttp.responseText);
+            document.getElementById('result').innerHTML = obj.message;
+        }
+    }
+    xhttp.open("POST",'https://3d1pftib26.execute-api.eu-west-1.amazonaws.com/dev/hello', true);
+    xhttp.setRequestHeader ("Content-Type", "application/json;charset=UTF-8");
+
+    let name = {name: document.getElementById('name').value};
+    xhttp.send(JSON.stringify(name));
 
 }
